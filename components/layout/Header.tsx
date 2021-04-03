8a76dcc8abf68dc9daa0506369261cd00b6824e2
AngaBlue <angus@thebosmans.com>
2021-04-03 17:55:23 +1100

    Updated head

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 import LoginButton from "../LoginButton";
@@ -7,7 +6,7 @@ export default function Header(): JSX.Element {
     return (
         <header className="flex justify-between px-4 py-2 content-center border-b border-gray-100">
             <Link href="/">
-                <Image src="/banner.png" height={36} width={150} />
+                <img src="logo-banner.svg" alt="Logo Banner" />
             </Link>
             <LoginButton />
         </header>
