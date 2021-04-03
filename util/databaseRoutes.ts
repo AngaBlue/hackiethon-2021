ca0b944e6a269574c74df0777d8959d56a61d997
billytheme <mitchellreid02@gmail.com>
2021-04-03 22:05:27 +1100

    Initial database retrieval functions

---
+++ util/databaseRoutes.ts
@@ -0,0 +1,18 @@
+// Returns username based off nextAuthID
+function getUsername(nextAuthID: number): string {
+    console.error("getUsername function is incomplete");
+    return;
+}
+
+function getUserFriends(nextAuthID: number): Array<number> {
+    console.error("getUserFriends function is incomplete");
+    return;
+}
+
+function getUserIDByNextAuthAccessToken(nextAuthAccessToken: string): number {
+    // This should find the appropriate access token from the "sessions" table and return the corresponding user_id
+    console.error("getUserIDByNextAuthAccessToken function is incomplete");
+    return;
+}
+
+export { getUsername, getUserFriends };
