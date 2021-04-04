3bcfe22a27482c205742164df4e73ccbcae8bf4d
jl91569 <10607947+jl91569@users.noreply.github.com>
2021-04-04 22:31:47 +1000

    oops

--- util/database/deleteEvent.ts
+++ util/database/deleteEvent.ts
@@ -1,11 +1,19 @@
-import { Dayjs } from "dayjs";
-
-export function deleteEvent(nextAuthAccessToken: string, startTime: Dayjs, endTime: Dayjs): void {
+import connection from "./connection";
+import { getUserID } from "./getUserID";
+export function deleteEvent(nextAuthAccessToken: string, eventId: number): Promise<void> {
     // TODO: Change this to accept an event ID
     // Check if event ID has mapping to user ID
     // If so, remove mapping and check if any further mappings to that event ID exist
     // If none, then delete event
     // Otherwise, event should remain in db as it's being used by other users
+
+    const userId = getUserID(nextAuthAccessToken);
+
+    connection.query(
+        "DELETE FROM user_events WHERE event_id = 1 AND user_id = 18; \
+        DELETE FROM calendar_events WHERE id = 1 and (SELECT COUNT(*) FROM user_events WHERE event_id = 1) = 0;",
+        [eventId, userId, eventId, eventId]
+    );
     console.error("deleteEvent function is incomplete");
     return;
 }
