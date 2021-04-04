fa3a5d6ba12d66db0324d09164f22c31c40a0870
AngaBlue <angus@thebosmans.com>
2021-04-04 16:33:51 +1000

    Added UserCard

---
+++ components/UserCard.tsx
@@ -0,0 +1,15 @@
+import { HTMLAttributes } from "react";
+
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
+    name: string;
+    image: string;
+}
+
+export default function Card({ name, image, ...props }: CardProps): JSX.Element {
+    return (
+        <div {...props} className={`flex items-center ${props.className || ""}`}>
+            <img className="h-10 w-10 rounded-full mr-2 inline-block" alt={`${name}'s Avatar`} src={image} />
+            <span className="font-bold">@{name}</span>
+        </div>
+    );
+}
