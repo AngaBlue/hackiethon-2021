049b1a7cf304587b1a56ee72f88907f27eeb65ee
AngaBlue <angus@thebosmans.com>
2021-04-04 16:53:36 +1000

    Privacy & TOS

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -1,3 +1,12 @@
 export default function privacy(): JSX.Element {
-    return <div></div>;
+    return (
+        <div className="p-4">
+            <h1 className="font-bold text-5xl mb-4">Privacy</h1>
+            <p>
+                We don&apos;t have a privacy policy yet but we can assure you we&apos;re not bad people.
+                <br />
+                xoxo
+            </p>
+        </div>
+    );
 }
--- pages/tos.tsx
+++ pages/tos.tsx
@@ -1,3 +1,12 @@
 export default function tos(): JSX.Element {
-    return <div></div>;
+    return (
+        <div className="p-4">
+            <h1 className="font-bold text-5xl mb-4">Terms of Service</h1>
+            <p>
+                We don&apos;t have a terms of service yet we hope that you&apos;ll be a nice person.
+                <br />
+                Thanks!
+            </p>
+        </div>
+    );
 }
