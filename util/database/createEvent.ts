3a94cd1f01d3976324544fcb1fe9b3bbbde41e78
jl91569 <10607947+jl91569@users.noreply.github.com>
2021-04-05 00:37:45 +1000

    UNTESTED: Return event details on createEvent

--- util/database/createEvent.ts
+++ util/database/createEvent.ts
@@ -1,11 +1,16 @@
 import { Dayjs } from "dayjs";
 
+import { CalendarEvents } from "../../types/database";
 import connection from "./connection";
 import { getUserID } from "./getUserID";
 
 const MYSQL_DATE_TIME_FORMAT = "YYYY-MM-DD HH:mm:ss";
 
-export async function createEvent(nextAuthAccessToken: string, startTime: Dayjs, endTime: Dayjs): Promise<number> {
+export async function createEvent(
+    nextAuthAccessToken: string,
+    startTime: Dayjs,
+    endTime: Dayjs
+): Promise<CalendarEvents> {
     const userId = await getUserID(nextAuthAccessToken);
     // Create new event and persist event ID
     // Use event ID to create new mapping between user and event
@@ -21,14 +26,14 @@ export async function createEvent(nextAuthAccessToken: string, startTime: Dayjs,
 
     // let db_utcOffset = `${Math.floor(st_utcOffset / 60)}${st_utcOffset % 60}`
 
-    const new_event_id = await connection.query(
-        "INSERT INTO calendar_events (external_id, start_time, end_time, utc_offset) VALUES(?, ?, ?, ?); SELECT LAST_INSERT_ID()",
-        [null, db_startTime, db_endTime, st_utcOffset]
+    const event_results = await connection.query(
+        "INSERT INTO calendar_events (external_id, start_time, end_time, utc_offset) VALUES(?, ?, ?, ?); \
+        SET @new_event_id := (SELECT LAST_INSERT_ID()); \
+        INSERT INTO user_events VALUES(?, @new_event_id, 1); \
+        SELECT * FROM calendar_events WHERE id = @new_event_id;",
+        [null, db_startTime, db_endTime, st_utcOffset, userId]
     );
-
-    await connection.query("INSERT INTO user_events VALUES(?, ?, 1)", [userId, new_event_id[1]]);
-
     await connection.end();
 
-    return;
+    return event_results[3];
 }
