9d28290942770829374a85c350769833efccd6b7
jl91569 <10607947+jl91569@users.noreply.github.com>
2021-04-04 23:53:30 +1000

    Fix bad query string argument in deleteEvent

--- pages/api/calendar/deleteEvent.ts
+++ pages/api/calendar/deleteEvent.ts
@@ -6,8 +6,8 @@ import { deleteEvent } from "../../../util/database/deleteEvent";
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
     const session = await getSession({ req });
 
-    if (session?.accessToken && typeof req.query.friendID == "string") {
-        deleteEvent(session.accessToken, Number(req.query.friendID));
+    if (session?.accessToken && typeof req.query.eventID == "string") {
+        deleteEvent(session.accessToken, Number(req.query.eventID));
         res.status(200).json({});
     } else {
         res.status(500).json({});
