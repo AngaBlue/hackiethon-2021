90ff42fad0efac8839dbd480899aed141dadbfbb
jl91569 <10607947+jl91569@users.noreply.github.com>
2021-04-04 21:08:55 +1000

    Update getUserFriends and getFriendRequests

--- util/database/getFriendRequests.ts
+++ util/database/getFriendRequests.ts
@@ -1,23 +1,20 @@
-import { UserRelationship } from "../../types/database";
 import connection from "./connection";
+import { User, UserAuth } from "../../types/database";
 import { getUserID } from "./getUserID";
 import { getUsernameByID } from "./getUsernameByID";
 
-export async function getFriendRequests(nextAuthAccessToken: string): Promise<Array<string>> {
+export type FriendRequestData = Pick<User, "id" | "username"> & Pick<UserAuth, "image">
+
+export async function getFriendRequests(nextAuthAccessToken: string): Promise<Array<FriendRequestData>> {
     const userID = await getUserID(nextAuthAccessToken);
-    const results: Array<
-        Partial<UserRelationship>
-    > = await connection.query("SELECT main FROM int_user_relationships WHERE confirmed = false AND secondary = ?", [
-        userID
-    ]);
+    const results: Array<FriendRequestData> = await connection.query(
+        "SELECT u.id, u.name AS username, u.image FROM social_motion_dev.int_user_relationships AS rel \
+        INNER JOIN users AS u \
+            ON rel.secondary = ? AND rel.confirmed = FALSE AND rel.main = u.id",
+        [userID]
+    );
 
     await connection.end();
 
-    const output = [];
-
-    for (const result of results) {
-        output.push(await getUsernameByID(result.main));
-    }
-
-    return output;
+    return results;
 }
--- util/database/getUserFriends.ts
+++ util/database/getUserFriends.ts
@@ -1,17 +1,19 @@
-import { User } from "../../types/database";
+import { User, UserAuth } from "../../types/database";
 import connection from "./connection";
 
-export type Friend = Pick<User, "id" | "username">;
+export type Friend = Pick<User, "id" | "username"> & Pick<UserAuth, "image">
 
 export async function getUserFriends(nextAuthAccessToken: string): Promise<Friend[]> {
     const results: Friend[] = (
         await connection.query(
             "SET @user_id := (SELECT user_id FROM sessions WHERE (session_token = ? OR access_token = ?)); \
-        SELECT int_users.id, int_users.username \
+        SELECT int_users.id, int_users.username, u.image \
         FROM int_users \
         INNER JOIN int_user_relationships AS rel \
             ON (int_users.id = rel.main AND rel.main = @user_id) \
             OR (int_users.id = rel.secondary AND rel.main = @user_id) \
+        INNER JOIN users AS u \
+            ON u.id = int_users.id \
         WHERE int_users.id <> @user_id;",
             [nextAuthAccessToken, nextAuthAccessToken]
         )
