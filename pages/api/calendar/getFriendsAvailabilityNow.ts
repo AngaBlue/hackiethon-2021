f38d44f74c1e3a6e264ecd2378bf60916ddb6756
jl91569 <10607947+jl91569@users.noreply.github.com>
2021-04-04 20:51:58 +1000

    Fix getFriendsAvailabilityNow

--- pages/api/calendar/getFriendsAvailabilityNow.ts
+++ pages/api/calendar/getFriendsAvailabilityNow.ts
@@ -2,56 +2,67 @@ import dayjs from "dayjs";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
 
-import { UserAuth } from "../../../types/database";
+import { CalendarEventsWithUserInfo, UserAuth } from "../../../types/database";
 import { getUserEvents } from "../../../util/database/getUserEvents";
 import { getUserFriends } from "../../../util/database/getUserFriends";
 import { getUserImage } from "../../../util/database/getUserImage";
 import { getUsernameByID } from "../../../util/database/getUsernameByID";
+import { getFriendEventsWithUserInfo } from "../../../util/database/getFriendEventsWithUserInfo";
+import { UserWithNestedEvents } from "../../../types/util";
 
 export type getFriendsAvailabilityResponse = Pick<UserAuth, "name" | "image">[];
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
     const accessToken = (req.query.access as string) || (await getSession({ req }))?.accessToken || null;
     if (accessToken) {
-        const friends = await getUserFriends(accessToken);
-        const availableFriends = [];
-        for (const friend of friends) {
-            let available = true;
-            const friendEvents = await getUserEvents(friend.id);
-            // const friendRecurringEvents = await getUserRecurringEvents(friend);
+        const availableFriends :UserWithNestedEvents[] = new Array();
 
-            for (const event of friendEvents) {
+        const friendEvents = (await getFriendEventsWithUserInfo(accessToken));
+
+        let friendEventsSorted = friendEvents.sort((a, b) => (a.user_id > b.user_id) ? 1 : -1);
+        let friendsByEvent :UserWithNestedEvents[] = friendEventsSorted.reduce((prev, curr) => {
+            let filtered_results = prev.filter((p) => p.user_id == curr.user_id);
+            let current_obj :UserWithNestedEvents;
+
+            if (filtered_results.length == 0) {
+                current_obj = new UserWithNestedEvents(curr.user_id, curr.username, curr.image);
+                prev.push(current_obj);
+            }
+            else {
+                current_obj = filtered_results[1];
+            }
+
+            current_obj.events.push(
+            {
+                "id": curr.id,
+                "external_id": curr.external_id,
+                "start_time": curr.start_time,
+                "end_time": curr.end_time,
+                "utc_offset": curr.utc_offset
+            });
+            
+            return prev;
+        }, new Array());
+
+        for (const friend of friendsByEvent) {
+            let available = true;
+            for (const event of friend.events) {
                 if (dayjs(event.start_time).isBefore(dayjs()) && dayjs(event.end_time).isAfter(dayjs())) {
                     available = false;
                 }
             }
-
-            // for (const event of friendRecurringEvents) {
-            //     if (
-            //         event.startTime.isBefore(dayjs()) &&
-            //         event.endTime.isAfter(dayjs()) &&
-            //         event.dayOfWeek == dayjs().day()
-            //     ) {
-            //         available = false;
-            //     }
-            // }
 
             if (available) {
                 availableFriends.push(friend);
             }
         }
 
-        const availableFriendUsernames: string[] = [];
-        const availableFriendPictures: string[] = [];
-
-        for (const id of availableFriends) {
-            availableFriendUsernames.push(await getUsernameByID(id));
-            availableFriendPictures.push(await getUserImage(id));
-        }
-
         res.status(200).json(
-            availableFriendUsernames.map((e, i) => {
-                return { name: e, image: availableFriendPictures[i] };
+            availableFriends.map(f => {
+                return {
+                    name: f.username,
+                    image: f.image
+                };
             })
         );
     } else {
