5ee4b3ddeae809118cac8c3653e32ee5e969e20c
AngaBlue <angus@thebosmans.com>
2021-04-04 16:19:19 +1000

    Added dashboard cards

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,7 +4,7 @@ import LoginButton from "../LoginButton";
 
 export default function Header(): JSX.Element {
     return (
-        <header className="flex justify-between px-4 py-2 content-center border-b border-gray-100">
+        <header className="flex justify-between px-4 py-2 content-center border-b border-blue-100">
             <Link href="/">
                 <img src="/logo-banner.svg" alt="Logo Banner" className="cursor-pointer" />
             </Link>
--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -1,3 +1,47 @@
+import Link from "next/link";
+import { useSession } from "next-auth/client";
+import React from "react";
+
+interface navItem {
+    name: string;
+    path: string;
+    authenticated?: true;
+}
+
+const navItems: navItem[] = [
+    {
+        name: "Dashboard",
+        path: "/dashboard"
+    },
+    {
+        name: "Personal Timetable",
+        path: "/timetable"
+    },
+    {
+        name: "Friends",
+        path: "/friends"
+    },
+    {
+        name: "Settings",
+        path: "/settings"
+    }
+];
+
 export default function Nav(): JSX.Element {
-    return <nav className="border-r border-gray-100"></nav>;
+    const [session] = useSession();
+
+    return (
+        <nav className="border-r border-blue-100">
+            {navItems.map((item) => {
+                if (item.authenticated && !session) return null;
+                return (
+                    <Link key={item.path} href={item.path}>
+                        <div className="flex p-4 items-center w-full text-blue-900 hover:bg-blue-100 cursor-pointer transition-all duration-500 border-b border-blue-100">
+                            <span className="font-bold block h-auto">{item.name}</span>
+                        </div>
+                    </Link>
+                );
+            })}
+        </nav>
+    );
 }
