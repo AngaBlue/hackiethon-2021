87bff348d5e60fe8effbb6ea95cb1c94d20e42b8
jl91569 <10607947+jl91569@users.noreply.github.com>
2021-04-04 19:36:28 +1000

    Fix some SQL queries and .gitattributes

--- pages/api/user/getUserFriends.ts
+++ pages/api/user/getUserFriends.ts
@@ -1,13 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
 
-import { getUserFriends } from "../../../util/databaseRoutes";
+import { getUserFriends } from "../../../util/database/getUserFriends";
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
     const session = await getSession({ req });
 
     if (session?.accessToken) {
-        res.status(200).json(getUserFriends(session.accessToken));
+        res.status(200).json(await getUserFriends(session.accessToken));
     } else {
         res.status(500).json({});
     }
---
+++ util/database/getFriendEvents.ts
@@ -0,0 +1,28 @@
+import { UserRelationship } from "../../types/database";
+import { User } from "../../types/database";
+import connection from "./connection";
+import { getUserID } from "./getUserID";
+import { CalendarEvents } from "../../types/database";
+
+export async function getFriendEvents(nextAuthAccessToken: string): Promise<Array<CalendarEvents>> {
+    console.log(nextAuthAccessToken);
+    const results: Array<CalendarEvents> = (
+        await connection.query(
+            "SET @user_id := (SELECT user_id FROM sessions WHERE (session_token = ? OR access_token = ?)); \
+            SELECT ce.*, int_users.* \
+            FROM int_users \
+            INNER JOIN int_user_relationships AS rel \
+                ON (int_users.id = rel.main AND rel.main = @user_id) \
+                OR (int_users.id = rel.secondary AND rel.main = @user_id) \
+            INNER JOIN user_events AS ue \
+                ON (ue.user_id = int_users.id) \
+            INNER JOIN calendar_events as ce \
+                ON ce.id = ue.event_id",
+            [nextAuthAccessToken, nextAuthAccessToken]
+        )
+    )[1];
+
+    await connection.end();
+
+    return results;
+}
