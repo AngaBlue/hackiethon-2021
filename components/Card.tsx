767ff17422875c46c936aa0284d6e93b9c3e0e2d
AngaBlue <angus@thebosmans.com>
2021-04-04 16:33:40 +1000

    Updated missing className handling

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,7 +6,7 @@ interface CardProps extends HTMLAttributes<HTMLDivElement> {
 
 export default function Card({ title, ...props }: CardProps): JSX.Element {
     return (
-        <div {...props} className={`rounded-lg bg-gray-100 overflow-hidden ${props.className}`}>
+        <div {...props} className={`rounded-lg bg-gray-100 overflow-hidden ${props.className || ""}`}>
             {!!title && (
                 <div className="bg-blue-600 p-4">
                     <h2 className="text-white text-2xl font-bold">{title}</h2>
