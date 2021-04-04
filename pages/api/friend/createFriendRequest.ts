018ce3bce2fec30647d38db1ea93375b28c9578c
billytheme <mitchellreid02@gmail.com>
2021-04-04 21:56:07 +1000

    Fix deletFriendRequest, change createFriendRequest

--- pages/api/friend/createFriendRequest.ts
+++ pages/api/friend/createFriendRequest.ts
@@ -8,16 +8,16 @@ import { requestNewFriend } from "../../../util/database/requestNewFriend";
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
     const session = await getSession({ req });
 
-    if (session?.accessToken && typeof req.query.friendID == "string") {
-        if (await doesUserFriendExist(session.accessToken, Number(req.query.friendID))) {
+    if (session?.accessToken && typeof req.query.username == "string") {
+        if (await doesUserFriendExist(session.accessToken, req.query.username)) {
             res.status(400).json({ message: "User is already your friend" });
             return;
         }
-        if (await doesUserRequestExist(session.accessToken, Number(req.query.friendID))) {
+        if (await doesUserRequestExist(session.accessToken, req.query.username)) {
             res.status(400).json({ message: "This request already exists" });
             return;
         }
-        requestNewFriend(session.accessToken, Number(req.query.friendID));
+        requestNewFriend(session.accessToken, req.query.username);
         res.status(200).json({});
     } else {
         res.status(500).json({});
--- util/database/deleteFriendRequest.ts
+++ util/database/deleteFriendRequest.ts
@@ -1,10 +1,8 @@
 import connection from "./connection";
-import { getIDByUsername } from "./getIDByUsername";
 import { getUserID } from "./getUserID";
 
-export async function deleteFriendRequest(nextAuthAccessToken: string, friendUsername: string): Promise<void> {
+export async function deleteFriendRequest(nextAuthAccessToken: string, friendID: number): Promise<void> {
     const userID = await getUserID(nextAuthAccessToken);
-    const friendID = await getIDByUsername(friendUsername);
     connection.query(
         "DELETE FROM int_user_relationships WHERE (main = ? AND secondary = ?) OR (main = ? AND secondary = ?)",
         [friendID, userID, userID, friendID]
