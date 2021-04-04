8a779853669b7605cfb52887f64172bb9956d0f3
jl91569 <10607947+jl91569@users.noreply.github.com>
2021-04-04 23:10:31 +1000

    Fix typing by importing a different type

--- util/database/getUserProfile.ts
+++ util/database/getUserProfile.ts
@@ -1,10 +1,11 @@
 import { User } from "../../types/database";
 import connection from "./connection";
+import { Friend } from "./getUserFriends";
 
-export async function getUserProfile(nextAuthAccessToken: string): Promise<string> {
-    const results: Array<
-        Partial<User>
-    > = await connection.query(
+
+
+export async function getUserProfile(nextAuthAccessToken: string): Promise<Friend> {
+    const results: Array<Friend> = await connection.query(
         "SELECT sessions.user_id, int_users.username, int_users.image \
         FROM int_users \
         INNER JOIN sessions \
@@ -16,7 +17,7 @@ export async function getUserProfile(nextAuthAccessToken: string): Promise<strin
     await connection.end();
 
     if (results.length != 1) {
-        return "";
+        // should probably throw or something no idea at this point
     }
 
     return results[0];
